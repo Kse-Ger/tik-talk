83af9a5e58b1401a4464867cb9ad4cb55e77da54
Gerasimcuka <ksenija.gerasimcuka@accenture.com>
2024-07-15 18:05:25 +0300

    Added connection to backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
+import { ProfileService } from './data/services/profile.service';
+import { CommonModule } from '@angular/common';
+import { Profile } from './data/interfaces/profile.interfaces';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet, ProfileCardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tik-talk';
+  profileService = inject(ProfileService)
+  profiles: Profile[] = []
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe(val => {
+      this.profiles = val
+    })
+  }
 }
