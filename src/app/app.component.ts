96ccba700290974265b51ba54bdad7995909b43c
Gerasimcuka <ksenija.gerasimcuka@accenture.com>
2024-07-16 14:26:11 +0300

    Finished profile cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
 import { ProfileService } from './data/services/profile.service';
-import { CommonModule } from '@angular/common';
 import { Profile } from './data/interfaces/profile.interfaces';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent, CommonModule],
+  imports: [RouterOutlet, ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
